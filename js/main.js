097eb01300fcf8acc1b72e7b9ef9ed2c15ef7c06
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-05-01 17:13:50 +0200

    campominato funzione creaQuadrato

--- js/main.js
+++ js/main.js
@@ -1,20 +1,29 @@
 let grid = document.getElementById("griglia");
 
-
-
 for ( let i = 1 ; i <= 100 ; i++ ) {
+
+    let cella = creaQuadrato(i);
+    cella.classList.add("square");
+
+    grid.append(cella);
+}
+
+function creaQuadrato(x){
 
     let quadrato = document.createElement("div");
     quadrato.classList.add("square");
-
-    grid.append(quadrato);
-
+    
     quadrato.addEventListener("click" , function(){
      
         quadrato.classList.toggle("evidenziato")
-        quadrato.innerHTML = i
-        console.log(quadrato , i )
 
+        if (quadrato.innerHTML === '') {
+            quadrato.innerHTML = x;
+        } else {
+            quadrato.innerHTML = '';
+        }
+        console.log(quadrato , x )
     })
-}
 
+    return quadrato;
+}
