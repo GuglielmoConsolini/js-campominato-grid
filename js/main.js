83ab8d0059038bd6cd568e9fc23e694f38b3807e
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-05-01 17:26:31 +0200

    campominato. funzione per creare quadrati e per l'eventlistener

--- js/main.js
+++ js/main.js
@@ -1,29 +1,33 @@
+// definisco la variabile che contiene i quadrati
 let grid = document.getElementById("griglia");
 
+// ciclo che gestisce la creazione e il click dei quadrati
 for ( let i = 1 ; i <= 100 ; i++ ) {
 
     let cella = creaQuadrato(i);
     cella.classList.add("square");
-
+    clickQuadrato(cella,i)
     grid.append(cella);
 }
 
+// funzione per creare i quadrati nel virtual DOM
 function creaQuadrato(x){
 
     let quadrato = document.createElement("div");
     quadrato.classList.add("square");
-    
-    quadrato.addEventListener("click" , function(){
-     
-        quadrato.classList.toggle("evidenziato")
-
-        if (quadrato.innerHTML === '') {
-            quadrato.innerHTML = x;
-        } else {
-            quadrato.innerHTML = '';
-        }
-        console.log(quadrato , x )
-    })
-
     return quadrato;
+}
+// funzione per gestire i click sui quadrati
+function clickQuadrato(quadrato,x){
+quadrato.addEventListener("click" , function(){
+     
+    quadrato.classList.toggle("evidenziato")
+
+    if (quadrato.innerHTML === '') {
+        quadrato.innerHTML = x;
+    } else {
+        quadrato.innerHTML = '';
+    }
+    console.log(quadrato , x )
+})
 }
