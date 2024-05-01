a7c0b9008bbcbd89786742d039006024b04a48cc
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-05-01 19:43:41 +0200

    Campominato Bonus 3 con modalità di difficoltà

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let pulsante = document.querySelector("button")
 
 // codice per impedire la creazione di altre griglie(mi sono aiutato con la documentazione su internet)
 let giocoAvviato = false
+
 // click per iniziare il gioco
 pulsante.addEventListener("click",function(){
     if(!giocoAvviato){
@@ -10,20 +11,49 @@ pulsante.addEventListener("click",function(){
         giocoAvviato = true
     } 
 })
-// funzione di inizio gioco
+
+
+
+// funzione di inizio gioco + modaltà hard, medium , easy
 function avviaGioco(){
 
     let grid = document.getElementById("griglia");
 
-    pulsante.removeEventListener("click" , avviaGioco)
+    // dichiaro come variabile la mia select , e ne dichiaro un altra che è uguale al suo Value
+    let selectDifficolta = document.getElementById("difficolta");
+    let difficoltàSelezionata = selectDifficolta.value;
+    
+    // if per gestire se il value è hard , in questo caso il ciclo stampa solo i quadrati necessari e rimuove le classi inutili
+    if(difficoltàSelezionata === "hard"){
 
-    for ( let i = 1 ; i <= 100 ; i++ ) {
+        grid.classList.remove("griglia-medium" , "griglia-easy")
+        for (let i = 1 ; i <= 49 ; i++){
+        let cella = creaQuadrato(i);
+        cella.classList.add("square");
+        clickQuadrato(cella,i)
+        grid.append(cella);
+        }
+    // stessa cosa per value medium
+    } else if(difficoltàSelezionata === "medium") { 
 
+    grid.classList.remove("griglia-hard" , "griglia-easy")
+    for ( let i = 1 ; i <= 81 ; i++ ) {
         let cella = creaQuadrato(i);
         cella.classList.add("square");
         clickQuadrato(cella,i)
         grid.append(cella);
     }
+    // uguale anche per value facile
+    } else if(difficoltàSelezionata === "easy"){
+
+        grid.classList.remove("griglia-hard" , "griglia-medium")
+        for ( let i = 1 ; i <= 100 ; i++ ) {
+        let cella = creaQuadrato(i);
+        cella.classList.add("square");
+        clickQuadrato(cella,i)
+        grid.append(cella);
+    }
+}
 }
 // funzione per creare i quadrati nel virtual DOM
 function creaQuadrato(x){
