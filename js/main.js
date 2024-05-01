052a034ceb1904932bf34a677ea8bac7dc423d6b
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-05-01 16:45:10 +0200

    campominato M3

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 let grid = document.getElementById("griglia");
 
+
+
 for ( let i = 1 ; i <= 100 ; i++ ) {
 
     let quadrato = document.createElement("div");
     quadrato.classList.add("square");
 
     grid.append(quadrato);
+
+    quadrato.addEventListener("click" , function(){
+     
+        quadrato.classList.toggle("evidenziato")
+        quadrato.innerHTML = i
+        console.log(quadrato , i )
+
+    })
 }
+
